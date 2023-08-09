4233dbb57fc7ad73dfb60926cf7f62ff59bfcd87
ilsegaertner <joachimpruegl@gmail.com>
2023-08-09 12:29:22 +0200

    Update MovieData

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,30 @@
+export const MovieView = ({movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <div>
+        <span>Year: </span>
+        <span>{movie.year}</span>
+      </div>
+      <div>
+        <span>Actors: </span>
+        <span>{movie.actors}</span>
+      </div>
+      <button onClick={onBackClick}>Back</button>
+    </div>
+  );
+};
