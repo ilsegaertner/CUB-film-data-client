4a1e1c42347b96f97504030e4738ca2262d68788
ilsegaertner <joachimpruegl@gmail.com>
2023-08-09 12:30:49 +0200

    move export keyword

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view"; // .jsx format ending not needed here
 
-export const MainView = () => {
+const MainView = () => {
   const [movies, setMovies] = useState([
     {
       id: 1,
