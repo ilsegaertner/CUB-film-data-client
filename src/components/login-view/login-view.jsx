1b66febdcad81a99ca6d27c7a9ca00a69e254be2
ilsegaertner <joachimpruegl@gmail.com>
2023-08-19 10:17:13 +0200

    Create login-view.jsx

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    // this prevents the default behaviour of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+    fetch("https://openlibrary.org/account/login.json", {
+      method: "POST",
+      body: JSON.stringify(data)
+    }).then((response) => {
+      if (response.ok) {
+        onLoggedIn(username);
+      } else {
+        alert("Login failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          minLength="5"
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          minLength="6"
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
