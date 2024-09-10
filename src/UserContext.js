e4ef17617cb919993d7384736b284be17fb01134
Ilse Gaertner <joachimpruegl@gmail.com>
2024-09-10 22:28:11 +0200

    refactor userContext

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,5 +1,4 @@
-import { User } from "lucide-react";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 // create a new context
 const UserContext = createContext();
@@ -9,13 +8,35 @@ export const useUserContext = () => useContext(UserContext);
 
 // Provider component to wrap around the app and provide state
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [favouriteMovies, setFavouriteMovies] = useState([]);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
+
+  // update local storage whenever user or token changes
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token", token);
+    }
+  }, [user, token]);
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+    localStorage.clear();
+  };
 
   return (
-    <UserContext.Provider
-      value={{ user, setUser, favouriteMovies, setFavouriteMovies }}
-    >
+    <UserContext.Provider value={{ user, setUser, token, setToken, logout }}>
       {children}
     </UserContext.Provider>
   );
--- src/components/favouriteHandler.js
+++ src/components/favouriteHandler.js
@@ -1,85 +1,132 @@
-export const addFavouriteHandler = (movieId, user, token, updateUser) => {
+import { useUserContext } from "../userContext";
+
+export const addFavouriteHandler = async (movieId, user, setUser, token) => {
+  // const { user, setUser } = useUserContext();
+
   if (!movieId) {
     console.error("Movie ID not found");
     return;
   }
 
-  fetch(
-    `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
+  try {
+    const response = await fetch(
+      `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Failed to add to favorites: ${errorMessage}`);
     }
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        return response.text().then((errorMessage) => {
-          throw new Error(`Failed to add to favorites: ${errorMessage}`);
-        });
-      }
-    })
-    .then((data) => {
-      if (data) {
-        updateUser();
 
-        console.log(user);
+    const updatedUser = await response.json();
 
-        // Show a success message or provide feedback to the user
-        // alert("Movie added to favorites");
-      }
-    })
-    .catch((error) => {
-      console.error("Error adding movie to favorites", error);
-    });
+    setUser(updatedUser);
+
+    console.log("Movie added to favourites", updatedUser);
+  } catch (error) {
+    console.error("Error adding movie to favorites", error);
+  }
+
+  // .then((response) => {
+  //   if (response.ok) {
+  //     return response.json();
+  //   } else {
+  //     return response.text().then((errorMessage) => {
+  //       throw new Error(`Failed to add to favorites: ${errorMessage}`);
+  //     });
+  //   }
+  // })
+  // .then((data) => {
+  //   if (data) {
+  //     updateUser();
+
+  //     console.log(user);
+
+  //     // Show a success message or provide feedback to the user
+  //     // alert("Movie added to favorites");
+  //   }
+  // })
+  // .catch((error) => {
+  //   console.error("Error adding movie to favorites", error);
+  // });
 };
 
-export const removeFavouriteHandler = (
-  movieId,
-  user,
-  token,
-  updateUser,
-  title
-) => {
+export const removeFavouriteHandler = async (movieId, user, setUser, token) => {
+  // const [user, setUser] = useUserContext();
+
   if (!movieId) {
     console.error("Movie ID not found");
     return;
   }
 
-  // Make a delete request to the API
-  const url = `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`;
-
-  fetch(url, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        // Filter out the movie from the user's favourite list
-        const updatedFavouriteMovies = user.FavouriteMovies.filter(
-          (favMovieId) => favMovieId !== title
-        );
-        // Update the user state locally to reflect the removal of the favorite movie
-        const updatedUser = {
-          ...user,
-          FavouriteMovies: updatedFavouriteMovies,
-        };
-        updateUser(updatedUser);
-
-        // User Feedback
-        // alert("Movie removed from favorites");
-      } else {
-        throw new Error("Failed to remove movie from favourites");
+  try {
+    const response = await fetch(
+      `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
       }
-    })
-    .catch((error) => {
-      console.error("Failed to remove movie", error);
-    });
+    );
+    if (!response.ok) {
+      throw new Error("Failed to remove movie from favourites");
+    }
+
+    const updatedFavouriteMovies = user.FavouriteMovies.filter(
+      (favMovieId) => favMovieId !== movieId
+    );
+
+    const updatedUser = {
+      ...user,
+      FavouriteMovies: updatedFavouriteMovies,
+    };
+
+    setUser(updatedUser);
+    console.log("Movie removed from favorites", updatedUser);
+  } catch (error) {
+    console.error("Error removing movie from favorites", error);
+  }
 };
+
+//   // Make a delete request to the API
+//   const url = `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`;
+
+//   fetch(url, {
+//     method: "DELETE",
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((response) => {
+//       if (response.ok) {
+//         // Filter out the movie from the user's favourite list
+//         const updatedFavouriteMovies = user.FavouriteMovies.filter(
+//           (favMovieId) => favMovieId !== title
+//         );
+//         // Update the user state locally to reflect the removal of the favorite movie
+//         const updatedUser = {
+//           ...user,
+//           FavouriteMovies: updatedFavouriteMovies,
+//         };
+//         updateUser(updatedUser);
+
+//         // User Feedback
+//         // alert("Movie removed from favorites");
+//       } else {
+//         throw new Error("Failed to remove movie from favourites");
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Failed to remove movie", error);
+//     });
+// };
