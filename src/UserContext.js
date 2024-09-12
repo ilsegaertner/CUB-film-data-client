22ade5f8c5e0afd0dc52d2296460a30c6620b401
Ilse Gaertner <joachimpruegl@gmail.com>
2024-09-12 18:56:52 +0200

    Update UserContext.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,9 +1,9 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 // create a new context
 const UserContext = createContext();
 
-// Custom hook to use the context easily in components
 export const useUserContext = () => useContext(UserContext);
 
 // Provider component to wrap around the app and provide state
@@ -13,13 +13,27 @@ export const UserProvider = ({ children }) => {
 
   const [user, setUser] = useState(storedUser ? storedUser : null);
   const [token, setToken] = useState(storedToken ? storedToken : null);
+  const [favouriteMovies, setFavouriteMovies] = useState(
+    user?.FavouriteMovies || []
+  );
 
   // update local storage whenever user or token changes
   useEffect(() => {
     if (user) {
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          id: user.id,
+          Username: user.Username,
+          Email: user.Email,
+          Birthday: user.Birthday,
+          FavouriteMovies: user.FavouriteMovies || [],
+        })
+      );
+      setFavouriteMovies(user.FavouriteMovies || []);
     } else {
       localStorage.removeItem("user");
+      setFavouriteMovies([]);
     }
 
     if (token) {
@@ -29,14 +43,77 @@ export const UserProvider = ({ children }) => {
     }
   }, [user, token]);
 
+  const toggleFavourites = async (movieId) => {
+    try {
+      if (favouriteMovies.includes(movieId)) {
+        const response = await fetch(
+          `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`,
+          {
+            method: "DELETE",
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            "Failed to remove from favorites: ",
+            response.statusText
+          );
+        }
+
+        setUser({
+          ...user,
+          FavouriteMovies: user.FavouriteMovies.filter((id) => id !== movieId),
+        });
+        toast.success("Movie removed from favorites!", {
+          toastId: "success1",
+        });
+      } else {
+        const response = await fetch(
+          `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/movies/${movieId}`,
+          {
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to add to favorites: ", response.statusText);
+        }
+
+        setUser({
+          ...user,
+          FavouriteMovies: [...user.FavouriteMovies, movieId],
+        });
+        toast.success("Movie added to favourites", {
+          toastId: "success1",
+        });
+      }
+    } catch (error) {
+      toast.error("Failed to update favourites!");
+      console.error("Error updating favourites:", error);
+    }
+  };
+
   const logout = () => {
     setUser(null);
     setToken(null);
+    setFavouriteMovies([]);
     localStorage.clear();
   };
 
   return (
-    <UserContext.Provider value={{ user, setUser, token, setToken, logout }}>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        token,
+        setToken,
+        favouriteMovies,
+        setFavouriteMovies,
+        toggleFavourites,
+        logout,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
