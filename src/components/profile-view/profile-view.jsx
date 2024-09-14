a7c1ea639abe37e67f57f72714e7c4bd8e4c7b6d
Ilse Gaertner <joachimpruegl@gmail.com>
2024-09-14 09:04:13 +0200

    Remove console logs

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -13,19 +13,9 @@ export const ProfileView = ({ movies }) => {
   const { user, setUser, token } = useUserContext();
 
   useEffect(() => {
-    console.log("User in ProfileView:", user); // Log the entire user object
-    console.log("Username:", user?.Username); // Check the Username field
-    console.log("Token:", token);
-
     if (!user || !user.Username || !token) {
-      console.log("No user or token available.", { user, token });
       return;
     }
-
-    console.log("Username:", user.Username); // Check the actual value of Username
-
-    // console.log("Token used for request:", token);
-    // console.log("User:", user);
 
     fetch(
       `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}`,
@@ -34,14 +24,12 @@ export const ProfileView = ({ movies }) => {
       }
     )
       .then((response) => {
-        console.log("Response:", response); // Check status
         if (!response.ok) {
           throw new Error("Failed to fetch user data");
         }
         return response.json();
       })
       .then((data) => {
-        console.log("Received data:", data); // Log the received data
         if (!data) {
           throw new Error("Received null or invalid data");
         }
@@ -57,8 +45,6 @@ export const ProfileView = ({ movies }) => {
         console.error("Error fetching user data", error);
       });
   }, [token]);
-
-  console.log(user);
 
   // Filters based on the user's favorite Movies array
   let favouriteMovieList = user?.FavouriteMovies?.length
