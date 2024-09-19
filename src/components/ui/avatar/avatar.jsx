96cea7fbb62af5715bda536e108a3764ff50f77e
Ilse Gaertner <joachimpruegl@gmail.com>
2024-09-19 17:59:25 +0200

    update avatar to use userContext state

--- src/components/ui/avatar/avatar.jsx
+++ src/components/ui/avatar/avatar.jsx
@@ -6,19 +6,21 @@ import { useUserContext } from "../../../UserContext";
 import imageUploadIcon from "../../../assets/icons/image-up.svg";
 
 const Avatar = () => {
-  const { user, setUser } = useUserContext();
+  const { user, setUser, token, avatar, setAvatar } = useUserContext();
   const { Username } = user || {};
 
-  const initials = Username
-    ? Username.split(" ")
-        .map((name) => name[0].toUpperCase())
-        .join("")
-    : "";
-  const [avatar, setAvatar] = useState(user?.Avatar || defaultAvatar);
+  // const initials = Username
+  //   ? Username.split(" ")
+  //       .map((name) => name[0].toUpperCase())
+  //       .join("")
+  //   : "";
+  // const [avatar, setAvatar] = useState(
+  //   user?.Avatar ? `data:image/*;base64,${user.Avatar}` : defaultAvatar
+  // );
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleLoadAvatar = (event) => {
+  const handleLoadAvatar = async (event) => {
     const file = event.target.files[0];
     if (file) {
       setLoading(true);
@@ -41,15 +43,48 @@ const Avatar = () => {
         return;
       }
 
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const newAvatar = reader.result;
+      const formData = new FormData();
+      formData.append("Avatar", file);
+
+      try {
+        const response = await fetch(
+          `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}/avatar`,
+          {
+            method: "PUT",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData,
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to upload avatar");
+        }
+
+        const updatedUser = await response.json();
+
+        const newAvatar = updatedUser.Avatar
+          ? `data:image/*;base64,${updatedUser.Avatar}`
+          : defaultAvatar;
+
         setAvatar(newAvatar);
-        setUser((prevUser) => ({ ...prevUser, Avatar: newAvatar }));
+        setUser(updatedUser);
         setError("");
+      } catch (error) {
+        console.error("Error uploading avatar:", error);
+        setError(error.message);
+      } finally {
         setLoading(false);
-      };
-      reader.readAsDataURL(file);
+      }
+
+      //   const reader = new FileReader();
+      //   reader.onloadend = () => {
+      //     const newAvatar = reader.result;
+      //     setAvatar(newAvatar);
+      //     setUser((prevUser) => ({ ...prevUser, Avatar: newAvatar }));
+      //     setError("");
+      //     setLoading(false);
+      //   };
+      //   reader.readAsDataURL(file);
     }
   };
 
@@ -62,7 +97,7 @@ const Avatar = () => {
           {avatar ? (
             <img src={avatar} alt="User Avatar" className="avatar-preview" />
           ) : (
-            <p>Hello {initials}</p>
+            <img src={defaultAvatar} />
           )}
 
           <input
@@ -73,7 +108,7 @@ const Avatar = () => {
             onChange={handleLoadAvatar}
           />
           <label htmlFor="file-input" className="custom-file-upload">
-            <img src={imageUploadIcon} width={20} />
+            <img src={imageUploadIcon} width={20} alt="upload-icon" />
           </label>
 
           {error && <p className="error">{error}</p>}
