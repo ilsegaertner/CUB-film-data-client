bf98cfa8965932a7aa50f7fbf1b4565796fecc5b
Ilse Gaertner <joachimpruegl@gmail.com>
2024-09-19 07:47:59 +0200

    remove user fetching from profileView

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,45 +6,44 @@ import { MovieCard } from "../movie-card/movie-card";
 import { UpdateUser } from "./update-user";
 import { DeleteProfile } from "./delete-profile";
 
-// import { useUserContext } from "../../userContext";
 import { useUserContext } from "../../UserContext";
 
 export const ProfileView = ({ movies }) => {
   const { user, setUser, token } = useUserContext();
 
-  useEffect(() => {
-    if (!user || !user.Username || !token) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!user || !user.Username || !token) {
+  //     return;
+  //   }
 
-    fetch(
-      `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (!data) {
-          throw new Error("Received null or invalid data");
-        }
-        setUser({
-          id: data._id,
-          Username: data.Username,
-          Email: data.Email,
-          Birthday: data.Birthday,
-          FavouriteMovies: data.FavouriteMovies || [],
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching user data", error);
-      });
-  }, [token]);
+  //   fetch(
+  //     `https://cub-film-data-dc72bcc7ff05.herokuapp.com/users/${user.Username}`,
+  //     {
+  //       headers: { Authorization: `Bearer ${token}` },
+  //     }
+  //   )
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch user data");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       if (!data) {
+  //         throw new Error("Received null or invalid data");
+  //       }
+  //       setUser({
+  //         id: data._id,
+  //         Username: data.Username,
+  //         Email: data.Email,
+  //         Birthday: data.Birthday,
+  //         FavouriteMovies: data.FavouriteMovies || [],
+  //       });
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching user data", error);
+  //     });
+  // }, [token]);
 
   // Filters based on the user's favorite Movies array
   let favouriteMovieList = user?.FavouriteMovies?.length
